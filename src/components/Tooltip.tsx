f250b039fe70d2329def7a7950ac506abd1a085a
Bekasyl <devbekasyl@gmail.com>
2025-02-05 18:21:16 +0500

    fix: tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -13,7 +13,7 @@ export default function Tooltip({ text, children }: TooltipProps) {
 
   return (
     <div 
-      className="relative inline-block"
+      className="relative inline-block cursor-help"
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
     >
@@ -24,13 +24,19 @@ export default function Tooltip({ text, children }: TooltipProps) {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 10 }}
-            className="absolute z-50 w-[700px] h-[10vh] bg-white text-black flex items-center justify-center text-center rounded-3xl shadow-lg -top-20 left-0 -translate-x-1/2"
+            className="fixed z-[9999] w-[90vw] md:w-[700px] h-auto min-h-[10vh] 
+              bg-white text-black flex items-center justify-center text-center 
+              rounded-3xl shadow-lg p-4 md:p-6
+              left-[10%] -translate-x-1/2 top-[40%]"
           >
-            <div className="text-xl whitespace-nowrap font-medium">
+            <div className="text-base md:text-xl font-medium">
               {text}
             </div>
             {/* Arrow */}
-            <div className="absolute -bottom-2 left-1/2 -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-white" />
+            <div className="absolute -bottom-2 left-1/2 -translate-x-1/2 w-0 h-0 
+              border-l-8 border-r-8 border-t-8 
+              border-l-transparent border-r-transparent border-t-white" 
+            />
           </motion.div>
         )}
       </AnimatePresence>
